7490204364175a6a51429a4a7bac06b0312f0033
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-08 20:44:35 +0300

    PHP within HTML

--- www/index.php
+++ www/index.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hello</title>
+</head>
+<body>
+    <h1>Simple HTML</h1>
 <?php
-echo "Hello PhpProject!";
+echo "<div class='myclass'> Anything goes here</div>"
+?>
+
+<footer>NOT done by PHP</footer>
+
+</body>
+</html>
