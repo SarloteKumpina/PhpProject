84a49e7c582b5db5a7c4700b3c76652fbf4b1252
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-10 09:35:24 +0300

    added require once ../

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once "..src/templates/header.php";
-require_once "..src/utilities.php";
+require_once "../src/templates/header.php";
+require_once "../src/utilities.php";
 echo "<h1>My PHP page</h1>";
 $arr = ["Valdis", "Pēteris", "Līga"];
 makeUnorderedList($arr);
@@ -9,5 +9,5 @@ echo "<div class='results'>" . myAdder(5,200) . "</div>";
 echo "<div class='mycontainer'>";
 printFizzBuzz(30);
 echo"</div>";
-require_once "..src/templates/footer.html";
+require_once "../src/templates/footer.html";
 
