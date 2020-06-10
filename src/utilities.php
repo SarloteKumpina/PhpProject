0d16ada1a418154b7f70cdd5a641a7e64d67b61d
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-10 09:31:38 +0300

    added fizzbuzz

--- src/utilities.php
+++ src/utilities.php
@@ -6,3 +6,27 @@ function makeUnorderedList($myArr) {
     }
     echo "</ul>";
 }
+
+function myAdder($a, $b) {
+    return $a+$b;
+}
+
+function printFizzBuzz($max=20) {
+    for ($i = 0; $i <$max; $i++) {
+             
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            $className = "fizzbuzz";
+            $text = "FIZZBUZZ";
+        } elseif ($i % 3 === 0) {
+            $className = "fizz";
+            $text = "FIZZ";
+        } elseif ($i % 5 === 0) {
+            $className = "buzz";
+            $text = "BUZZ";
+        } else {
+            $className = "number";
+            $text = $i;
+    }
+    echo "<div class = 'box $className'>$text</div>";
+    }
+}
--- www/index.php
+++ www/index.php
@@ -1,5 +1,13 @@
 <?php
 require_once "..src/templates/header.php";
+require_once "..src/utilities.php";
 echo "<h1>My PHP page</h1>";
+$arr = ["Valdis", "Pēteris", "Līga"];
+makeUnorderedList($arr);
+
+echo "<div class='results'>" . myAdder(5,200) . "</div>";
+echo "<div class='mycontainer'>";
+printFizzBuzz(30);
+echo"</div>";
 require_once "..src/templates/footer.html";
 
