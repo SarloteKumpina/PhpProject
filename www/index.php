837bc50e455303d9efb6f5f0a69caed426f58065
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-10 08:52:55 +0300

    index.php changes require_once header and footer

--- www/index.php
+++ www/index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Hello</title>
-</head>
-<body>
-    <h1>Simple HTML</h1>
 <?php
-echo "<div class='myclass'>Anything goes here</div>"
-?>
-<h2>Heading 2</h2>
-<?php
-    for ($i = 0; $i < 5; $i++) {
-        echo "<p>Paragraph No. $i</p>";
-    }
-?>
+require_once "..src/templates/header.php";
+echo "<h1>My PHP page</h1>";
+require_once "..src/templates/footer.html";
 
-<footer>NOT done by PHP</footer>
-
-</body>
-</html>
